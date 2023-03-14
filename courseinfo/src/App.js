fd7306e4aa01f9df6f5cfa2872a647bdccd88ced
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-14 19:49:11 -0300

    up

---
+++ courseinfo/src/App.js
@@ -0,0 +1,57 @@
+//modo one line
+//const Header = ({course}) => <h1>{course}</h1>
+
+const Header = (props) =>{
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) =>{
+  const {part1} = props
+  const {exercises1} = props
+  const {part2} = props
+  const {exercises2} = props
+  const {part3} = props
+  const {exercises3} = props
+  return(
+    <div>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  const {exercises1} = props
+  const {exercises2} = props
+  const {exercises3} = props
+
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content 
+          part1 = {part1} exercises1 = {exercises1}
+          part2 = {part2} exercises2 = {exercises2}
+          part3 = {part3} exercises3 = {exercises3}
+        />  
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+    </div>
+  )
+}
+
+export default App
